d609bfbc8803e6a3e3323a0cb91ab723ae34db55
Vestina Bertasiute <vestina.bertasiute@gmail.com>
2025-02-06 14:08:38 +0200

    initial commit

---
+++ js/api.js
@@ -0,0 +1,12 @@
+export async function fetchText() {
+    try {
+        const response = await fetch("https://random-word-api.herokuapp.com/word?number=42");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching text:", error);
+        return "This is a fallback text for testing. Fetching from API failed.";
+    }
+}
+
+fetchText();
