1badb8a39f9dd629656653e768f445d0ca7b7605
Vestina Bertasiute <vestina.bertasiute@gmail.com>
2025-02-06 15:06:31 +0200

    spanning text and adding colors when user types characters

--- js/script.js
+++ js/script.js
@@ -2,24 +2,47 @@ import { fetchText } from './api.js';
 
 const textDisplay = document.getElementById("text-display");
 const timeDisplay = document.getElementById("time");
+const userInput = document.getElementById("user-input");
 
 let originalText = "";
 let timer = 60;
+let interval;
+let totalCharsTyped = 0;
+let correctChars = 0;
 
 async function startTest() {
     originalText = await fetchText();
-    textDisplay.textContent = originalText;
+    textDisplay.innerHTML = originalText.split("").map(char => `<span>${char}</span>`).join("");
 }
 
 function startTimer() {
-    setInterval(() => {
+    interval = setInterval(() => {
         timer--;
         timeDisplay.textContent = timer;
         if (timer === 0) {
             clearInterval(interval);
-            endTest();
         }
     }, 1000);
 }
+
+userInput.addEventListener("input", () => {
+    if (timer === 60) startTimer();
+
+    const typedText = userInput.value;
+    totalCharsTyped = typedText.length;
+
+    const textSpans = textDisplay.querySelectorAll("span");
+    correctChars = 0;
+    console.log(originalText[0]);
+
+    for (let i = 0; i < typedText.length; i++) {
+        if (typedText[i] === originalText[i]) {
+            textSpans[i].style.color = "green";
+            correctChars++;
+        } else {
+            textSpans[i].style.color = "red";
+        }
+    }
+
+});
 startTest();
-startTimer();
