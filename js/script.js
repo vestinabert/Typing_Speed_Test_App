59ceb8ba830ea2616d02a90d492a77822a8ab2b8
Vestina Bertasiute <vestina.bertasiute@gmail.com>
2025-02-06 14:38:10 +0200

    adding timer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 import { fetchText } from './api.js';
 
 const textDisplay = document.getElementById("text-display");
+const timeDisplay = document.getElementById("time");
+
 let originalText = "";
+let timer = 60;
+
 async function startTest() {
     originalText = await fetchText();
-    textDisplay.innerText = originalText;
+    textDisplay.textContent = originalText;
 }
 
+function startTimer() {
+    setInterval(() => {
+        timer--;
+        timeDisplay.textContent = timer;
+        if (timer === 0) {
+            clearInterval(interval);
+            endTest();
+        }
+    }, 1000);
+}
 startTest();
+startTimer();
